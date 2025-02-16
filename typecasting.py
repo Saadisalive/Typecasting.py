4a9c05253348661a8ff8033746388ea1456eade4
Saadisalive <snafiullah@gmail.com>
2025-02-16 19:53:33 +0500

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,27 @@
+#assining Different Variables
+name = "penguin"
+age = 15
+is_student = True
+weight = 38.5
+
+#printing Different Variables And their DAta types 
+print("name :",name)
+print("data type of Name is",type(name))
+
+print("age :",age)
+print("Data type of Age is",type(age))
+
+print("is_student :",is_student)
+print("Data type os is_student is",type(is_student))
+
+print("weight :",weight)
+print("Data Type of weight is",type(weight))
+
+#type casting to convert the datatype of variables
+print("\n After Type Casting....")
+age =str(age)
+print(age)
+print("Typetype of age is",type(age))
+weight=int(weight)
+print(weight)
+print("Datatype of Weight is",type(weight))
